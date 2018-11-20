3ef96a8cf384ded79200d23a80da337c922bd962
abadaa <abadaa@mail.gvsu.edu>
2018-11-20 20:52:33 +0000

    technically skipped some tdd steps, but i refactored to use objects. could have broken this down more though

--- add-fractions/add_fractions.py
+++ add-fractions/add_fractions.py
@@ -1,2 +1,6 @@
-def add_frac(a, b):
-    return b
+class Fractions:
+    def __init__(self, num, den=None):
+        self.num = num
+        self.den = den
+    def add(self, self2):
+        return self.num + self2.num
