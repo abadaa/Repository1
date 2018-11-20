5d19fe3b974a1b7e77b76deeee97a08f87d83dc0
abadaa <abadaa@mail.gvsu.edu>
2018-11-20 21:21:44 +0000

    adding two integers

--- add-fractions/add_fractions_test.py
+++ add-fractions/add_fractions_test.py
@@ -11,6 +11,10 @@ class testadd(unittest.TestCase):
     def test_add_zero_plus_five(self):
         result = Fractions(0).add(Fractions(5))
         self.assertEqual(result, 5)
+    def test_add_three_plus_four(self):
+        result = Fractions(3).add(Fractions(4))
+        self.assertEqual(result, 7)
+    
         
 if __name__ == '__main__':
      unittest.main()
